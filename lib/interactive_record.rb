232d276e7ef8c2ab67db380393a9ac8b55846091
thomas dubiel <github email address>
2020-03-03 22:45:53 -0600

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,6 +2,7 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+#generalized database table
 
   def self.table_name
     self.to_s.downcase.pluralize
